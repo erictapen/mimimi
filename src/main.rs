64782e91b06e1a3c5d599e03c860839149c5ad26
Justin Humm <justin.humm@posteo.de>
2017-09-21 00:54:38 +0200

    rule 1 works now as intended, tests for rule 1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,42 @@
 use std::fmt;
 
+// run `cargo test` for these
+#[cfg(test)]
+mod tests {
+    use Theorem;
+    use Symbol;
+
+
+    #[test]
+    fn test_rule_1_appends_u() {
+        let mi: Theorem = Theorem(vec![Symbol::M,
+                                        Symbol::I,
+        ]);
+        let miu: Theorem = Theorem(vec![Symbol::M,
+                                        Symbol::I,
+                                        Symbol::U,
+        ]);
+        assert!(mi.rule_1() == Some(miu));
+    }
+
+    #[test]
+    fn test_rule_1_fails_on_u_as_last_one() {
+        let miu: Theorem = Theorem(vec![Symbol::M,
+                                        Symbol::I,
+                                        Symbol::U,
+        ]);
+        assert!(miu.rule_1() == None);
+    }
+    #[test]
+    fn test_rule_1_is_okay_on_empty_theorem() {
+        let empty: Theorem = Theorem(vec![]);
+        assert!(empty.rule_1() == None);
+    }
+}
+
 #[derive(Debug)]
 #[derive(PartialEq)]
+#[derive(Clone)]
 enum Symbol {
     M,
     I,
@@ -18,7 +53,8 @@ impl fmt::Display for Symbol {
     }
 }
 
-struct Theorem(Vec<Symbol>);
+#[derive(PartialEq)]
+pub struct Theorem(Vec<Symbol>);
 
 impl fmt::Display for Theorem {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -34,28 +70,29 @@ impl fmt::Display for Theorem {
 }
 
 /*
- * In this part, the 4 transformation rules are implemented by four rust functions.
+ * In this part, the four transformation rules are implemented by four rust functions.
  */
 
 impl Theorem {
-    // Rule 1: `xI -> xIU` or every String ending on I can be appended by an U.
+    /// Rule 1: `xI -> xIU` or every String ending on I can be appended by an U.
     fn rule_1(&self) -> Option<Theorem> {
-        if &self.0[&self.0.len() - 1] == &Symbol::I {
-            Some(Theorem(vec![Symbol::U]))
+        if !&self.0.is_empty() && &self.0[&self.0.len() - 1] == &Symbol::I {
+            let mut result = self.0.to_vec();
+            result.push(Symbol::U);
+            Some(Theorem(result))
         } else {
             None
         }
     }
-    
 }
 
 
 fn main() {
-    let x = Theorem(vec![Symbol::M,
+    let mi = Theorem(vec![Symbol::M,
              Symbol::I,
         //   Symbol::U,
         ]);
-    match x.rule_1() {
+    match mi.rule_1() {
         None => println!("{}", "Fail"),
         Some(theorem) => println!("{}", theorem),
     }
